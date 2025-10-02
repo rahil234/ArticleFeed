fae9a678b3b7d1951d06882075fe3fa3e5638d6d
Rahil <rahilsardar234@gmail.com>
2025-10-02 08:46:33 +0530

    Initial Commit

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT;
+    if (!port) {
+        throw new Error('PORT environment variable is not set');
+    }
+    await app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
+
+bootstrap();
---
+++ frontend/src/app/feeds/page.tsx
@@ -0,0 +1,7 @@
+export default function Feeds() {
+    return (
+        <div>
+            feeds
+        </div>
+    );
+}
