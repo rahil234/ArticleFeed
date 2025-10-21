6d712769b76a4c119d251be7b0feac9294097350
Rahil <rahilsardar234@gmail.com>
2025-10-21 09:52:28 +0530

    Refactor services and controllers to use dependency injection and improve type definitions

--- backend/src/article/interaction/domain/interaction.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Interaction } from '@/article/interaction/domain/interaction.entity';
-
-export interface InteractionRepository {
-    create(interaction: Interaction): Promise<Interaction>;
-    findAll(): Promise<Interaction[]>;
-    findByArticleId(articleId: string): Promise<Interaction[] | null>;
-    findManyByUserId(userId: string): Promise<Interaction[] | null>;
-    delete(id: string): Promise<void>;
-}
